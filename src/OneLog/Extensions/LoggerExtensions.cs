a5311efeddd25aa4b80f8348808eed1b68d73b71
viktorshevchenko210 <viktorshevchenko210@gmail.com>
2021-01-10 02:59:10 +0200

    updated SaveLogMiddleware

--- src/OneLog/Extensions/LoggerExtensions.cs
+++ src/OneLog/Extensions/LoggerExtensions.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Hosting;
 using OneLog.Infrastructure;
 using OneLog.Middleware;
 using OneLog.Models;
